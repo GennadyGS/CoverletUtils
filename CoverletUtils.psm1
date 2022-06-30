47e371c7d61ed94f61861e6c1ab6f161898b2d84
Gennadii Saltyshchak <GennadyGS@gmail.com>
2022-06-30 12:26:38 +0300

    Transform to PS module

---
+++ CoverletUtils.psm1
@@ -0,0 +1,22 @@
+﻿dotnet tool update -g coverlet.console
+dotnet tool update -g dotnet-reportgenerator-globaltool
+
+#Get public and private function definition files.
+$Public = Get-ChildItem $PSScriptRoot\*.ps1 -ErrorAction SilentlyContinue
+#$Private = Get-ChildItem $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue
+
+#Dot source the files
+Foreach ($import in @($Public)) {
+    Try {
+        #PS2 compatibility
+        if ($import.fullname) {
+            . $import.fullname
+        }
+    }
+    Catch {
+        Write-Error "Failed to import function $($import.fullname): $_"
+    }
+}
+
+#Create some aliases, export public functions
+Export-ModuleMember -Function $($Public | Select-Object -ExpandProperty BaseName)
---
+++ Invoke-TestsWithCoverletReport.ps1
@@ -0,0 +1,38 @@
+﻿Function Invoke-TestsWithCoverletReport {
+    param (
+        $framework = "net6.0",
+        $resultPath = "./TestResult/",
+        $includeNameSpaces,
+        $excludeNameSpaces
+    )
+
+    $testAssembly = ".\bin\Debug\$framework\*.dll"
+    $reportPath = "$resultPath/Report"
+
+    if ($includeNameSpaces) {
+        $includeParam = $includeNameSpaces
+    }
+    else {
+        $includeParam = "[*]*"
+    }
+
+    if ($excludeNameSpaces) {
+        $excludeParam = $excludeNameSpaces
+    }
+    else {
+        $excludeParam = "[]"
+    }
+
+    If (Test-Path $resultPath) { Remove-Item $resultPath -Recurse }
+
+    coverlet $testAssembly `
+        --target "dotnet" `
+        --targetargs "test --no-build" `
+        --format cobertura `
+        --output $resultPath `
+        --include $includeParam `
+        --exclude $excludeParam
+
+    reportgenerator "-reports:$resultPath\coverage.cobertura.xml" "-targetdir:$reportPath"
+    Start-Process $reportPath\index.html
+}
